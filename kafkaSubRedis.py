63d74ff51b6740f2a00b300eb870a3dec8ff1a84
William Metcalf <metcalwd@metcalwd-w540.rose-hulman.edu>
2018-05-03 13:29:50 -0400

    wip

--- kafkaSubRedis.py
+++ kafkaSubRedis.py
@@ -1,13 +1,17 @@
 import redis
+import json
+# import msgpack
 from kafka import KafkaConsumer
 db = None
 
 def listen(consumer):
     for msg in consumer:
-        print(msg)
+        data = json.loads(msg)
+        print(data)
 
 if __name__ == '__main__':
     consumer = KafkaConsumer('redis', bootstrap_servers='137.112.89.91:9092', group_id='RedisReg', auto_offset_reset='latest')
     db = redis.StrictRedis(charset="utf-8", decode_responses=True)
-    print(db.ping())
+    assert(db.ping() == True)
+
     listen(consumer)
