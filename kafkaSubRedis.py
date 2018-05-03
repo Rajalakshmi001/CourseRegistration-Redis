2195927d33f0c2deedb84216f3838dbed0d434d2
William Metcalf <metcalwd@metcalwd-w540.rose-hulman.edu>
2018-05-03 13:25:07 -0400

    wip

--- kafkaSubRedis.py
+++ kafkaSubRedis.py
@@ -1,5 +1,6 @@
+import redis
 from kafka import KafkaConsumer
-
+db = None
 
 def listen(consumer):
     for msg in consumer:
@@ -7,4 +8,6 @@ def listen(consumer):
 
 if __name__ == '__main__':
     consumer = KafkaConsumer('redis', bootstrap_servers='137.112.89.91:9092', group_id='RedisReg', auto_offset_reset='latest')
+    db = redis.StrictRedis(charset="utf-8", decode_responses=True)
+    print(db.ping())
     listen(consumer)
