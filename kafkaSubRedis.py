a21ed4ea0bf431815a19714117880915ff4bdd19
William Metcalf <metcalwd@metcalwd-w540.rose-hulman.edu>
2018-05-03 13:33:18 -0400

    wip

--- kafkaSubRedis.py
+++ kafkaSubRedis.py
@@ -6,11 +6,14 @@ db = None
 
 def listen(consumer):
     for msg in consumer:
-        data = json.loads(msg.content.decone('utf-8'))
-        print(data)
+        print(msg)
 
 if __name__ == '__main__':
-    consumer = KafkaConsumer('redis', bootstrap_servers='137.112.89.91:9092', group_id='RedisReg', auto_offset_reset='latest')
+    consumer = KafkaConsumer('redis', 
+    bootstrap_servers='137.112.89.91:9092',
+     group_id='RedisReg', 
+        auto_offset_reset='latest',
+        value_deserializer= lambda m: json.loads(m.decode('utf-8')))
     db = redis.StrictRedis(charset="utf-8", decode_responses=True)
     assert(db.ping() == True)
 
